61ce88c7f0cb9741000b4089aa66d09c9bf56325
takaishotaro <takaishotaro326@gmail.com>
2022-12-20 22:50:29 +0900

    implement network layer

---
+++ network.tf
@@ -0,0 +1,17 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source             = "terraform-aws-modules/vpc/aws"
+  name               = "${var.env_code}-vpc"
+  cidr               = var.vpc_cidr
+  azs                = data.aws_availability_zones.available.names[*]
+  private_subnets    = var.private_cidr
+  public_subnets     = var.public_cidr
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "True"
+  }
+}
